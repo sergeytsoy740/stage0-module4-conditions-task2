322a4af49bf77293261182c6af74cae1b26cdf56
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-07-11 11:47:41 +0600

    Module #4.1. Task #2. Boolean Conditions

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int res = first > second ? first : second;
+        System.out.println(res);
     }
 }
